d8257134379c37a370d47c8e72335d1a8f9ecee8
Igor Kozyrenko <igor@ikseek.com>
2022-08-01 11:30:45 +0300

    Try to fix windows incompatibility

--- pipe_child.py
+++ pipe_child.py
@@ -5,9 +5,10 @@ import sys
 read_side_id = int(sys.argv[1])
 write_side_id = int(sys.argv[2])
 print("Child started with pipe r", read_side_id, "w", write_side_id, file=sys.stderr)
+print("Child attaching to the read side of the pipe", file=sys.stderr)
 read_side = os.fdopen(read_side_id, "r")
-print("Child closing write side id", file=sys.stderr)
-os.fdopen(write_side_id, "w").close()
+print("Child closing write side file descriptor", file=sys.stderr)
+os.close(write_side_id)
 print("Child wating on read", file=sys.stderr)
 read_side.read()
 print("Child terminating", file=sys.stderr) # You should defenitely see this line
--- pipe_parent.py
+++ pipe_parent.py
@@ -8,12 +8,16 @@ read_side_id, write_side_id = os.pipe()
 print("Parent created pipe: r", read_side_id, "w", write_side_id, file=sys.stderr)
 write_file_object = os.fdopen(write_side_id, "w")
 if sys.version_info[0] > 2:
-    os.set_inheritable(read_side_id, True)
-    os.set_inheritable(write_side_id, True)
+    if sys.platform == "win32":
+        os.set_handle_inheritable(read_side_id, True)
+        os.set_handle_inheritable(write_side_id, True)
+    else:
+        os.set_inheritable(read_side_id, True)
+        os.set_inheritable(write_side_id, True)
 
 python = sys.executable
 print("Parent starting child", file=sys.stderr)
-child = Popen([python, "pipe_child.py", str(read_side_id), str(write_side_id)], close_fds=False)
+child = Popen([python, "pipe_child.py", str(read_side_id), str(write_side_id)])
 print("Parent goes to sleep", file=sys.stderr)
 sleep(1)
 if len(sys.argv) == 2 and sys.argv[1] == "crash":
