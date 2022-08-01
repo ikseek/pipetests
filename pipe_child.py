42bfa10e91fe9e266f1895c8d82f23fc972a65aa
Igor Kozyrenko <igor@ikseek.com>
2022-08-01 11:00:08 +0300

    Fix python2 incompatibilities

--- pipe_child.py
+++ pipe_child.py
@@ -3,8 +3,11 @@ import os
 import sys
 
 read_side_id = int(sys.argv[1])
-print("Child started with pipe", read_side_id)
+write_side_id = int(sys.argv[2])
+print("Child started with pipe r", read_side_id, "w", write_side_id, file=sys.stderr)
 read_side = os.fdopen(read_side_id, "r")
-print("Child wating on read")
+print("Child closing write side id", file=sys.stderr)
+os.fdopen(write_side_id, "w").close()
+print("Child wating on read", file=sys.stderr)
 read_side.read()
-print("Child terminating")
+print("Child terminating", file=sys.stderr) # You should defenitely see this line
--- pipe_parent.py
+++ pipe_parent.py
@@ -5,20 +5,23 @@ from subprocess import Popen
 from time import sleep
 
 read_side_id, write_side_id = os.pipe()
-print("Parent created pipe: r", read_side_id, "w", write_side_id)
+print("Parent created pipe: r", read_side_id, "w", write_side_id, file=sys.stderr)
 write_file_object = os.fdopen(write_side_id, "w")
 if sys.version_info[0] > 2:
     os.set_inheritable(read_side_id, True)
+    os.set_inheritable(write_side_id, True)
 
 python = sys.executable
-print("Parent starting child")
-child = Popen([python, "pipe_child.py", str(read_side_id)], close_fds=False)
-print("Parent goes to sleep")
+print("Parent starting child", file=sys.stderr)
+child = Popen([python, "pipe_child.py", str(read_side_id), str(write_side_id)], close_fds=False)
+print("Parent goes to sleep", file=sys.stderr)
 sleep(1)
 if len(sys.argv) == 2 and sys.argv[1] == "crash":
-    print("Parent crashing")
+    print("Parent crashing", file=sys.stderr)
     raise Exception
 else:
-    print("Parent closing write side")
+    print("Parent closing write side", file=sys.stderr)
     write_file_object.close()
-    print("Parent exiting")
+    print("Parent waiting on child", file=sys.stderr)
+    child.wait()
+    print("Parent exiting", file=sys.stderr)
