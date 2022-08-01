831231bbe66e966762783606a17c06b6086819ea
Igor Kozyrenko <igor@ikseek.com>
2022-08-01 10:26:44 +0300

    Rename stdin example to stdin_*py

--- stdin_parent.py
+++ stdin_parent.py
@@ -6,7 +6,7 @@ from time import sleep
 
 python = sys.executable
 print("Parent starting child", file=sys.stderr)
-child = Popen([python, "child.py"], stdout=PIPE, stdin=PIPE)
+child = Popen([python, "stdin_child.py"], stdout=PIPE, stdin=PIPE)
 print("Parent started the child, reading the port number", file=sys.stderr)
 port = int(child.stdout.readline())
 print("Parent received port number from child:", port, file=sys.stderr)
