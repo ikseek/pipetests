c4efd97876bdc071b1db532a52f389b39b5c70ea
ran.itzhaki <ran.itzhaki@applitools.com>
2022-08-11 09:30:57 +0300

    Using `process.stdin` stream
    Instead of `fs.read` `process.stdin`

--- stdin_child.js
+++ stdin_child.js
@@ -1,33 +1,23 @@
 fs = require('fs')
 
-let pipe_read = 0;
-
-function read_to_eof(err, n, bytes) {
-    if (err) {
-        if (err.code === 'EOF') {
-            console.error("Child reached pipe EOF as error, happens on windows");
-            console.error("Child terminating");
-            process.exit(0);
-        } else {
-            console.error("Child pipe read error %s", err);
-        }
-    } else if (n === 0) {
-        console.error("Child reached pipe EOF normally");
-        console.error("Child terminating");
-        process.exit(0);
-    } else {
-        console.error("Child read %s bytes: %s", n, bytes);
-        fs.read(pipe_read, read_to_eof);
-    }
-}
-
 console.error("Child started");
 console.error("Child outputs dummy listen port")
 console.log("42");
 console.error("Child is waiting for EOF in pipe");
-fs.read(pipe_read, read_to_eof);
+// register to `end` event of `stdin` stream
+process.stdin.on('end',(err)=>{
+    if (err) {
+        console.error("Child pipe read error %s", err);
+        process.exit(1);
+    } else {
+        console.error("Child end");
+        process.exit(0);
+    }
+})
+// need to trigger the data to catch the `end` event
+process.stdin.on('data', () => {});
 
 function should_not_run() {
-  console.error("Child should not print it, should terminate earlier");
+    console.error("Child should not print it, should terminate earlier");
 }
 setTimeout(should_not_run, 5000);
